da21531c2252727e59b87533e1fd588861492440
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2025-09-09 11:22:55 -0600

    Creacion del Home controller

---
+++ src/main/java/org/esfe/Controladores/HomeController.java
@@ -0,0 +1,16 @@
+package org.esfe.Controladores;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return new String("Hola mundo");
+    }
+    
+
+}
