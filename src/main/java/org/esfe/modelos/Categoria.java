8185d758aff66a067e480eb969c8d466052de26c
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2025-09-12 23:51:56 -0500

    Creacion del modelo y sus Dtos de Noticia

--- src/main/java/org/esfe/modelos/Categoria.java
+++ src/main/java/org/esfe/modelos/Categoria.java
@@ -3,6 +3,7 @@ package org.esfe.modelos;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +14,7 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Noticia> noticias;
 }
