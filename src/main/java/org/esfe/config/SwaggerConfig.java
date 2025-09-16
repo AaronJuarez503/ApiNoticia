7f5723282d359a3b8a7f81015231771674b34713
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2025-09-15 20:26:05 -0500

    configuracion de swagger

---
+++ src/main/java/org/esfe/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package org.esfe.config;
+
+import org.springframework.http.HttpHeaders;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Gestión de Proyectos API",
+        description = "API Rest que permite el registro y mantenimiento de los datos de los proyectos.",
+        version = "1.0.0"
+    ),
+    security = @SecurityRequirement(
+        name = "Security Token"
+    )
+)
+
+@SecurityScheme(
+    name = "Security Token",
+    description = "Acceso a la aplicación mediante Tokens JWT",
+    type = SecuritySchemeType.HTTP,
+    paramName = HttpHeaders.AUTHORIZATION,
+    in = SecuritySchemeIn.HEADER,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+
+public class SwaggerConfig {
+}
