c13ef7ed8f87e317365194b08fa9ed3dd0e344da
JAZMIN EUGENIA LUE DURAN <ld25001@esfe.agape.edu.sv>
2025-09-16 10:59:30 -0600

    SCRUM-12 creacion de controladores de Noticia

---
+++ src/main/java/org/esfe/Controladores/NoticiaController.java
@@ -0,0 +1,93 @@
+package org.esfe.Controladores;
+
+import java.util.List;
+
+import org.esfe.dtos.noticia.NoticiaGuardar;
+import org.esfe.dtos.noticia.NoticiaModificar;
+import org.esfe.dtos.noticia.NoticiaSalida;
+import org.esfe.servicios.interfaces.INoticiaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/noticias")
+public class NoticiaController {
+
+    @Autowired
+    private INoticiaService noticiaService;
+
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR','USUARIO')")
+    @GetMapping
+    public ResponseEntity<Page<NoticiaSalida>> mostrarTodosPaginados(Pageable pageable){
+        Page<NoticiaSalida> noticias = noticiaService.obtenerTodosPaginado(pageable);
+
+        if(noticias.hasContent()){
+            return ResponseEntity.ok(noticias);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR','USUARIO')")
+    @GetMapping("/lista")
+    public ResponseEntity<List<NoticiaSalida>> mostrarTodos() {
+        List<NoticiaSalida> noticias = noticiaService.obtenerTodos();
+
+        if (!noticias.isEmpty()) {
+            return ResponseEntity.ok(noticias);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR','USUARIO')")
+    @GetMapping("/{id}")
+    public ResponseEntity<NoticiaSalida> mostrarPorId(@PathVariable Integer id){
+        NoticiaSalida noticia = noticiaService.obtenerPorId(id);
+
+        if(noticia != null){
+            return ResponseEntity.ok(noticia);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @PostMapping
+    public ResponseEntity<NoticiaSalida> crear(@RequestBody NoticiaGuardar noticiaGuardar){
+        NoticiaSalida noticia = noticiaService.crear(noticiaGuardar);
+
+        if(noticia != null){
+            return ResponseEntity.ok(noticia);
+        }
+
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @PutMapping("/{id}")
+    public ResponseEntity<NoticiaSalida> editar(@PathVariable Integer id, @RequestBody NoticiaModificar noticiaModificar) {
+        NoticiaSalida noticia = noticiaService.editar(noticiaModificar);
+
+        if (noticia != null) {
+            return ResponseEntity.ok(noticia);
+        }
+
+        return ResponseEntity.internalServerError().build();
+    }
+
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity eliminar(@PathVariable Integer id){
+        noticiaService.eliminarPorId(id);
+        return ResponseEntity.ok().body("Noticia eliminada correctamente");
+    }
+}
