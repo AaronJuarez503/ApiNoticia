61decbb7500da25c48bd32b045530910319c761c
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2025-09-16 10:48:05 -0600

    o

--- src/main/java/org/esfe/repositorios/INoticiaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.esfe.repositorios;
-
-import org.esfe.modelos.Noticia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface INoticiaRepository extends JpaRepository<Noticia, Integer> {
-}
